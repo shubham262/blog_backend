190c3d0d33b327cf997823aa41f282444f184149
Shubham <shubham.manohar@loveco.com>
2024-07-30 03:26:18 +0530

    bug fixes

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,15 +2,15 @@
 import validator from 'validator';
 import {
 	createNewUserService,
-	userExistService
+	userExistService,
 } from '../services/userService.js';
 import { apiErrorHandler } from '../helpers/errorHandler.js';
 import USER from '../models/User.js';
 
-const createNewUser = async (req, res, next) => { 
+const createNewUser = async (req, res, next) => {
 	try {
 		// const { username, email, password, phone, displayPicture, otp } = req.body;
-		
+
 		const response = await createNewUserService(req.body);
 
 		return res.status(201).json({
@@ -25,7 +25,7 @@ const createNewUser = async (req, res, next) => {
 const userExist = async (req, res, next) => {
 	try {
 		const { email } = req.body;
-		const userData = await userExistService(email);
+		const data = await userExistService(email);
 
 		res.status(201).json({
 			data,
@@ -35,67 +35,57 @@ const userExist = async (req, res, next) => {
 	}
 };
 
-const loginUser = async (req,res,next) => {
-	
-    try {
-        const {email, password} = req.body
+const loginUser = async (req, res, next) => {
+	try {
+		const { email, password } = req.body;
 
-		if(!email || !password){
-            return res.status(400).json({
-                success:false,
-                message: "Plz fill all the details carefully"
-            })
-        }
+		if (!email || !password) {
+			return res.status(400).json({
+				success: false,
+				message: 'Plz fill all the details carefully',
+			});
+		}
 
-        const User= await USER.findOne({email})
+		const User = await USER.findOne({ email });
 
-        const payload ={
-            email: User.email,
-            id: User._id
-        }
+		const payload = {
+			email: User.email,
+			id: User._id,
+		};
 
-        if(await bcrypt.compare(password,User.password)){
-            //if password matched
-             //now lets create a JWT token
-             let token = jwt.sign(payload, 
-                        process.env.JWT_SECRET,
-                        {expiresIn: "2h"}
-                        )
-            User = User.toObject()
-            User.token = token
-            
-            User.password = undefined
-            const options = {
-                expires: new Date( Date.now()+ 3*24*60*60*1000),
-                httpOnly: true  //It will make cookie not accessible on clinet side -> good way to keep hackers away
+		if (await bcrypt.compare(password, User.password)) {
+			//if password matched
+			//now lets create a JWT token
+			let token = jwt.sign(payload, process.env.JWT_SECRET, {
+				expiresIn: '2h',
+			});
+			User = User.toObject();
+			User.token = token;
 
-            }
-            res.cookie(
-                "token",
-                token,
-                options
-            ).status(200).json({
-                success: true,
-                token,
-                User,
-                message: "Logged in Successfully"
-
-            })
-
-        }else{
-            return res.status(403).json({
-                success: false,
-                message: "Password incorrects"
-            })
-        }
-
-    } catch (error) {
-        console.error(error)
-        res.status(500).json({
-            success: false,
-            message: "Login failure :" + error
-        })
-    }
-}
+			User.password = undefined;
+			const options = {
+				expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+				httpOnly: true, //It will make cookie not accessible on clinet side -> good way to keep hackers away
+			};
+			res.cookie('token', token, options).status(200).json({
+				success: true,
+				token,
+				User,
+				message: 'Logged in Successfully',
+			});
+		} else {
+			return res.status(403).json({
+				success: false,
+				message: 'Password incorrects',
+			});
+		}
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({
+			success: false,
+			message: 'Login failure :' + error,
+		});
+	}
+};
 
 export { userExist, createNewUser, loginUser };
